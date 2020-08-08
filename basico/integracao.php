9df599fcdbddf2abb68b8d42c0b18700945c3dca
Adriel Modollo <adrielmodollo@outlook.com>
2020-08-08 00:24:10 -0300

    Integrando PHP com CSS e HTML

--- basico/integracao.php
+++ basico/integracao.php
@@ -1,8 +1,34 @@
+<h1 center>
 <?php
 
-echo "Integração PHP \n";
-echo '<small> Menor </small>';
+echo "Integração PHP ";
+echo '<small azul> Menor </small>';
 
 ?>
+</h1>
 
-<div><button><?= Legal ?></button></div>
+<div center><button dobro><?= Legal ?></button></div>
+
+<style>
+    button {
+        padding: 5px  <?= 2 * 10 ?>px;
+        background-color: #4285f4;
+        color: #EEE;
+        font-weight: bold;
+        border-radius: 10px;
+    }
+
+    [center] {
+        display: flex;
+        justify-content: center;
+    }
+
+    [azul] {
+        color: #4286f4;
+    }
+
+    [dobro] {
+        font-size: 2rem;
+    }
+ 
+</style>
