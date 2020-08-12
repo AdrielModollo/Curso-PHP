16f392e92d61e774ddd173e9d9b81d78a9e91b12
Adriel Modollo <adrielmodollo@outlook.com>
2020-08-11 23:54:00 -0300

    Operadores Ternários

--- index.php
+++ index.php
@@ -24,69 +24,39 @@
                     <ul>
                         <li><a href="exercicio.php?dir=basico&file=ola">PHP básico</a></li>
                         <li><a href="exercicio.php?dir=basico&file=integracao">Integração</a></li>
-                    </ul>
-                    </div>
-                    <div class="modulo vermelho">
-                        <h3> Modulo 02- Básico </h3>
-                    <ul>
                         <li><a href="exercicio.php?dir=basico&file=desafio">Desafio PHP</a></li>
                         <li><a href="exercicio.php?dir=tipos&file=int">Tipo Inteiro </a></li>
                         <li><a href="exercicio.php?dir=tipos&file=float">Tipo Float</a></li>
-                    </ul>
-                    </div>
-                    <div class="modulo black">
-                        <h3> Modulo 03 - Básico </h3>
-                    <ul>
                         <li><a href="exercicio.php?dir=tipos&file=aritmeticas">Operações Aritméticas</A></a></li>
                         <li><a href="exercicio.php?dir=tipos&file=desafio_precedencia">Desafio Precedência</a></li>
                         <li><a href="exercicio.php?dir=tipos&file=string">Tipo String</a></li>
-                    </ul>
-                    </div>
-                    <div class="modulo laranja">
-                        <h3> Modulo 04 - Básico </h3>
-                    <ul>
                         <li><a href="exercicio.php?dir=tipos&file=desafio_string">Desafio String</A></a></li>
                         <li><a href="exercicio.php?dir=tipos&file=booleano">Tipo Booleano</a></li>
                         <li><a href="exercicio.php?dir=tipos&file=conversao">Conversões</a></li>
                     </ul>
                     </div>
                     <div class="modulo orangered">
-                        <h3> Modulo 05 - Básico </h3>
+                        <h3> Modulo 02 - Váriaveis </h3>
                     <ul>
                         <li><a href="exercicio.php?dir=variaveis&file=variaveis">Variáveis</a></li>
                         <li><a href="exercicio.php?dir=variaveis&file=desafio_equacao">Desafio equação</a></li>
                         <li><a href="exercicio.php?dir=variaveis&file=atribuicoes">Atribuições</a></li>
-                    </ul>
-                    </div>
-                    <div class="modulo gray">
-                        <h3> Modulo 06 - Básico </h3>
-                    <ul>
                         <li><a href="exercicio.php?dir=variaveis&file=interpolacao">Interpolação</A></a></li>
                         <li><a href="exercicio.php?dir=variaveis&file=variaveis_variaveis">Variáveis Variáveis</a></li>
                         <li><a href="exercicio.php?dir=variaveis&file=desafio_variaveis">Desafio Variáveis</a></li>
-                    </ul>
-                    </div>
-                    <div class="modulo blueviolet">
-                        <h3> Modulo 07 - Básico </h3>
-                    <ul>
                         <li><a href="exercicio.php?dir=variaveis&file=valor_referencia"> Valor Referência</a></li>
                         <li><a href="exercicio.php?dir=variaveis&file=constante">Constantes</a></li>
                     </ul>
                     </div>
                     <div class="modulo yellow">
-                        <h3> Modulo 08 - Básico </h3>
+                        <h3> Modulo 03 - Controle </h3>
                     <ul>
                         <li><a href="exercicio.php?dir=controle&file=if_else">IF And if_else</A></a></li>
                         <li><a href="exercicio.php?dir=controle&file=operadores_relacionais">Operadores Relacionais</a></li>
                         <li><a href="exercicio.php?dir=controle&file=desafio_pi">Desafio PI</a></li>
-                    </ul>
-                    </div>
-                    <div class="modulo yellowgreen">
-                        <h3> Modulo 09 - Básico </h3>
-                    <ul>
                         <li><a href="exercicio.php?dir=controle&file=operadores_logicos">Operadores Logicos</a></li>
-                        <li><a href="#">Exercicios B</a></li>
-                        <li><a href="#">Exercicios C</a></li>
+                        <li><a href="exercicio.php?dir=controle&file=desafio_opl">Desafio OPL</a></li>
+                        <li><a href="exercicio.php?dir=controle&file=operador_ternario">Operador Ternario</a></li>
                     </ul>
                     </div>
                            
