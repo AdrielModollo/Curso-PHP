4b6f3e20d78a6501794edd2d75dfb0ea7705ccff
Adriel Modollo <adrielmodollo@outlook.com>
2020-08-31 09:38:28 -0300

    Treinamento de função concluído

--- funcao/escopo.php
+++ funcao/escopo.php
@@ -1,34 +1,34 @@
-<div class="titulo"> Função & Escopo </div>
+<div class="titulo">Função & Escopo</div>
 
 <?php
-/*Coneceito de função: Sequência de códigos, algoritmos, passos, aonde é definido um nome de forma que você consiga chamar essa sequência
-para reutilização de códigos */
-    function imprimirMensagens(){   
-        echo "Olá";
-        echo "Até a próxima! <br> ";
-    }
+function imprimirMensagens() {
+    echo "Olá! ";
+    echo "Até a próxima!<br>";
+}
 
-    imprimirMensagens();
-    imprimirMensagens();
-    imprimirMensagens();
+imprimirMensagens();
+imprimirMensagens();
+imprimirMensagens();
 
-    $variavel = 1;
+$variavel = 1;
 
-    function trocaValor(){
-        $variavel = 2;
-        echo "Durante a função: $variavel <br>";
-    }
+function trocaValor() {
+    $variavel = 2;
+    echo "Durante a função: $variavel <br>";
+}
 
-    echo "Antes: $variavel <br>";
-    trocaValor();
-    echo "Depois: $variavel <br>";
+echo "Antes: $variavel <br>";
+trocaValor();
+echo "Depois: $variavel <br>";
 
-    function trocaValorDeVerdade() {
-        global $variavel;
-        $variavel = 3;
-        echo "Durante a função: $variavel <br>";
-    }
+function trocaValorDeVerdade() {
+    global $variavel;
+    $variavel = 3;
+    echo "Durante a função: $variavel <br>";
+}
 
-    echo "Antes: $variavel <br>";
-    trocaValorDeVerdade();
-    echo "Depois: $variavel <br>";
+echo "Antes: $variavel <br>";
+trocaValorDeVerdade();
+echo "Depois: $variavel <br>";
+
+var_dump(trocaValorDeVerdade());
